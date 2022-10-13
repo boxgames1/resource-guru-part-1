e91a64fd87b7f01e5fca6ea75f1a5649cfcc31dd
Oliver Alonso <alonso.oliverio@gmail.com>
2022-10-13 10:49:00 +0200

    add flat function implementation

--- index.ts
+++ index.ts
@@ -3,6 +3,17 @@ type FlatArray<Arr> = Arr extends ReadonlyArray<infer InnerArr>
   : Arr;
 
 const flatArray = <T>(array: Array<T>): FlatArray<Array<T>>[] => {
+  if (!Array.isArray(array)) throw new Error("Input is not an array");
+
+  const result = array.reduce(
+    (acc, item) => [
+      ...acc,
+      ...(Array.isArray(item) ? flatArray(item) : [item]),
+    ],
+    [] as Array<T>
+  );
+
+  return result;
 };
 
 export default flatArray;
